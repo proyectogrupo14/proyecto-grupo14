bab02cdfad4bf7cd2b346a484be44282c7fb5533
brunopauletti07 <brupau07@gmail.com>
2026-06-10 21:21:28 +0000

    correccion de lector y cantidad de varones y mujeres

--- proyecto.py
+++ proyecto.py
@@ -1,3 +1,4 @@
+import csv
 import codecs
 import streamlit as st
 st.title("Proyecto Grupal de programacion")
@@ -6,16 +7,29 @@ st.write("Empecemos a trabajar equipo!")
 
 #lectura del dataset
 escuelas = []
-escuelas_BA = open("establecimientos-educativos-12K.csv")
-for linea in escuelas_BA:
-    escuela = linea.strip().split(',')
-    escuelas.append(escuela)
+with open("establecimientos-educativos-12K.csv", newline='') as escuelas_ba:
+    lector = csv.reader(escuelas_ba)
 
+    for escuela in lector:
+        escuelas.append(escuela)
+
+
+#nenes en total
 def hombres_escuela():
     nenes = 0
     for escuela in escuelas[1:]:
-        nenes += int(escuela[26])
+        nenes = nenes + int(escuela[26])
     return nenes
-print(hombres_escuela()) 
+print("los varones son:", hombres_escuela())
+
+#nenas en total
+def mujeres_escuela():
+    nenas = 0
+    for escuela in escuelas[1:]:
+        nenas = nenas + int(escuela[27])
+    return nenas
+print("los mujeres son:", mujeres_escuela())
+
+
 
 
