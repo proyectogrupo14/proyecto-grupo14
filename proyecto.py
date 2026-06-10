83cf10217ddafec450ad7a88783c7fe0661fab4c
brunopauletti07 <brupau07@gmail.com>
2026-06-10 18:28:02 +0000

    Lectura del dataset y suma de varones general

--- proyecto.py
+++ proyecto.py
@@ -1,3 +1,19 @@
+import codecs
 import streamlit as st
 st.title("Proyecto Grupal de programacion")
 st.write("Empecemos a trabajar equipo!")
+
+
+#lectura del dataset
+escuelas = []
+escuelas_BA = open("establecimientos-educativos-12K.csv")
+for linea in escuelas_BA:
+    escuela = linea.strip().split(',')
+    escuelas.append(escuela)
+
+def hombres_escuela():
+    nenes = 0
+    for escuela in escuelas[1:]:
+        nenes += int(escuela[26])
+    return nenes
+print(hombres_escuela())
