f406ebc601469f58ce62be2a57adfd96be5d853a
BrenCastelli <brencastelli01@gmail.com>
2026-06-12 03:07:15 +0000

    Intenté crear el diccionario de listas.

--- proyecto.py
+++ proyecto.py
@@ -9,12 +9,71 @@ st.write("Empecemos a trabajar equipo!")'''
 
 
 #lectura del dataset
-escuelas = []
-with open("establecimientos-educativos-12K.csv", newline='') as escuelas_ba:
-    lector = csv.reader(escuelas_ba)
+#escuelas = []
+#with open("establecimientos-educativos-12K.csv", newline='') as escuelas_ba:
+#    lector = csv.reader(escuelas_ba)
+#
+#   for escuela in lector:
+#      escuelas.append(escuela)
 
-    for escuela in lector:
-        escuelas.append(escuela)
+with open('establecimientos-educativos-12K.csv', encoding="utf-8") as escuelas_ba:
+    escuelas_ba = csv.reader(escuelas_ba)
+
+    municipio_id=[]
+    municipio_nombre=[]
+    establecimiento_id=[]
+    establecimiento_nombre=[]
+    modalidad=[]
+    nivel=[]
+    direccion=[]
+    telefono=[]
+    email=[]
+    sector=[]
+    tipo_organizacion=[]
+    ambito=[]
+    matricula=[]
+    matricula_varones=[]
+    matricula_mujeres=[]
+    turnos=[]
+   
+    for escuela in escuelas_ba:
+        municipio_id.append(escuela[0])	
+        municipio_nombre.append(escuela[1])
+        establecimiento_id.append(escuela[2])
+        establecimiento_nombre.append(escuela[3])
+        modalidad.append(escuela[8])
+        nivel.append(escuela[9])
+        direccion.append(escuela[10])
+        telefono.append(escuela[11])
+        email.append(escuela[12])
+        sector.append(escuela[13])
+        tipo_organizacion.append(escuela[15])
+        ambito.append(escuela[17])
+        matricula.append(escuela[25])
+        matricula_varones.append(escuela[26])
+        matricula_mujeres.append(escuela[27])
+        turnos.append(escuela[29])
+        
+    diccionario={municipio_id[0]:municipio_id,
+                 municipio_nombre[0]:municipio_nombre,
+                 establecimiento_id[0]:establecimiento_id,
+                 establecimiento_nombre[0]:establecimiento_nombre,
+                 modalidad[0]:modalidad,
+                 nivel[0]:nivel,
+                 direccion[0]:direccion,
+                 telefono[0]:telefono,
+                 email[0]:email,
+                 sector[0]:sector,
+                 tipo_organizacion[0]:tipo_organizacion,
+                 ambito[0]:ambito,
+                 matricula[0]:matricula,
+                 matricula_varones[0]:matricula_varones,
+                 matricula_mujeres[0]:matricula_mujeres,
+                 turnos[0]:turnos}
+    print(diccionario)
+ 
+
+
  
 
 def niveles(nivel: str) -> list:
@@ -58,14 +117,14 @@ cantidad("Secundario",  ) =
 VARONES = 26
 MUJERES = 27
 
-print("Varones primaria:", cantidad("Nivel Primario", VARONES))
-print("Mujeres primaria:", cantidad("Nivel Primario", MUJERES))
+#print("Varones primaria:", cantidad("Nivel Primario", VARONES))
+#print("Mujeres primaria:", cantidad("Nivel Primario", MUJERES))
 
-print("Varones secundaria:", cantidad("Nivel Secundario", VARONES))
-print("Mujeres secundaria:", cantidad("Nivel Secundario", MUJERES))
+#print("Varones secundaria:", cantidad("Nivel Secundario", VARONES))
+#print("Mujeres secundaria:", cantidad("Nivel Secundario", MUJERES))
 
-print("Varones inicial:", cantidad("Nivel Inicial", VARONES))
-print("Mujeres inicial:", cantidad("Nivel Inicial", MUJERES))
+#print("Varones inicial:", cantidad("Nivel Inicial", VARONES))
+#print("Mujeres inicial:", cantidad("Nivel Inicial", MUJERES))
 
 
 
