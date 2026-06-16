98d9e7990f9241ce47145ab0bd151191a1b37c80
BrenCastelli <brencastelli01@gmail.com>
2026-06-16 20:33:00 +0000

    Funcion que contenga diccionario

--- proyecto.py
+++ proyecto.py
@@ -16,71 +16,79 @@ st.write("Empecemos a trabajar equipo!")'''
 #   for escuela in lector:
 #      escuelas.append(escuela)
 
-with open('establecimientos-educativos-prueba.csv', encoding="utf-8") as escuelas_ba:
-    escuelas_ba = csv.reader(escuelas_ba)
-
-    municipio_id=[]
-    municipio_nombre=[]
-    establecimiento_id=[]
-    establecimiento_nombre=[]
-    modalidad=[]
-    nivel=[]
-    direccion=[]
-    telefono=[]
-    email=[]
-    sector=[]
-    tipo_organizacion=[]
-    ambito=[]
-    matricula=[]
-    matricula_varones=[]
-    matricula_mujeres=[]
-    turnos=[]
+def estructura_datos():
+    with open('establecimientos-educativos-prueba.csv', encoding="utf-8") as escuelas_ba:
+        escuelas_ba = csv.reader(escuelas_ba)
+        municipio_id=[]
+        municipio_nombre=[]
+        establecimiento_id=[]
+        establecimiento_nombre=[]
+        modalidad=[]
+        nivel=[]
+        direccion=[]
+        telefono=[]
+        email=[]
+        sector=[]
+        tipo_organizacion=[]
+        ambito=[]
+        matricula=[]
+        matricula_varones=[]
+        matricula_mujeres=[]
+        turnos=[]
    
-    for escuela in escuelas_ba:
-        municipio_id.append(escuela[0])	
-        municipio_nombre.append(escuela[1])
-        establecimiento_id.append(escuela[2])
-        establecimiento_nombre.append(escuela[3])
-        modalidad.append(escuela[8])
-        nivel.append(escuela[9])
-        direccion.append(escuela[10])
-        telefono.append(escuela[11])
-        email.append(escuela[12])
-        sector.append(escuela[13])
-        tipo_organizacion.append(escuela[15])
-        ambito.append(escuela[17])
-        matricula.append(escuela[25])
-        matricula_varones.append(escuela[26])
-        matricula_mujeres.append(escuela[27])
-        turnos.append(escuela[29])
+        for escuela in escuelas_ba:
+            municipio_id.append(escuela[0])	
+            municipio_nombre.append(escuela[1])
+            establecimiento_id.append(escuela[2])
+            establecimiento_nombre.append(escuela[3])
+            modalidad.append(escuela[8])
+            nivel.append(escuela[9])
+            direccion.append(escuela[10])
+            telefono.append(escuela[11])
+            email.append(escuela[12])
+            sector.append(escuela[13])
+            tipo_organizacion.append(escuela[15])
+            ambito.append(escuela[17])
+            matricula.append(escuela[25])
+            matricula_varones.append(escuela[26])
+            matricula_mujeres.append(escuela[27])
+            turnos.append(escuela[29])
         
-    diccionario={municipio_id[0]:municipio_id[1:],
-                 municipio_nombre[0]:municipio_nombre[1:],
-                 establecimiento_id[0]:establecimiento_id[1:],
-                 establecimiento_nombre[0]:establecimiento_nombre[1:],
-                 modalidad[0]:modalidad[1:],
-                 nivel[0]:nivel[1:],
-                 direccion[0]:direccion[1:],
-                 telefono[0]:telefono[1:],
-                 email[0]:email[1:],
-                 sector[0]:sector[1:],
-                 tipo_organizacion[0]:tipo_organizacion[1:],
-                 ambito[0]:ambito[1:],
-                 matricula[0]:matricula[1:],
-                 matricula_varones[0]:matricula_varones[1:],
-                 matricula_mujeres[0]:matricula_mujeres[1:],
-                 turnos[0]:turnos[1:]}
+        diccionario ={municipio_id[0]:municipio_id[1:],
+                        municipio_nombre[0]:municipio_nombre[1:],
+                        establecimiento_id[0]:establecimiento_id[1:],
+                        establecimiento_nombre[0]:establecimiento_nombre[1:],
+                        modalidad[0]:modalidad[1:],
+                        nivel[0]:nivel[1:],
+                        direccion[0]:direccion[1:],
+                        telefono[0]:telefono[1:],
+                        email[0]:email[1:],
+                        sector[0]:sector[1:],
+                        tipo_organizacion[0]:tipo_organizacion[1:],
+                        ambito[0]:ambito[1:],
+                        matricula[0]:matricula[1:],
+                        matricula_varones[0]:matricula_varones[1:],
+                        matricula_mujeres[0]:matricula_mujeres[1:],
+                        turnos[0]:turnos[1:]}
+    
+    return diccionario
+
+#print(estructura_datos())
     
 def cantidad_valores(clave:str)->int:
     "Toma una clave del diccionario y evalua cuántos elementos posee la lista que representa su valor."
+
+    diccionario=estructura_datos()
     tamaño = 0
     for i in diccionario[clave]:
         tamaño+=1
     return tamaño
 
-print(cantidad_valores("nivel"))
+print(cantidad_valores("modalidad"))
 
 def tipos_valores(clave:str) -> list:
+
+    diccionario=estructura_datos()
     tipos_valores=[]
     for i in diccionario[clave]:
         if i not in tipos_valores:
